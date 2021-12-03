cf51ea424786a55d522baf69d6e5ed7a245261fe
Lars Froehlich <lars.froehlich@desy.de>
2021-12-03 11:06:42 +0100

    Add playground folder with LUA experiments
    Signed-off-by: Lars Froehlich <lars.froehlich@desy.de>

---
+++ playground/script.lua
@@ -0,0 +1,13 @@
+-- script.lua
+-- Receives a table, returns the sum of its components.
+io.write("The table the script received has:\n");
+x = 0
+for i = 1, #foo do
+  print(i, foo[i])
+  x = x + foo[i]
+end
+while true do
+    io.write("Infinite loop!\n");
+end
+io.write("Returning data back to C\n");
+return x
