0d4e520d92d7574f35e7d759ad94656ec27f8b7d
Lars Froehlich <lars.froehlich@desy.de>
2021-12-03 15:10:18 +0100

    Add LuaState class
    [why]
    A LUA state is a resource that should be handled
    carefully in order to avoid leaks. A proper C++
    move-only class can automatically take care of
    that.
    
    Signed-off-by: Lars Froehlich <lars.froehlich@desy.de>

---
+++ tests/test_LuaState.cc
@@ -0,0 +1,67 @@
+/**
+ * \file   test_LuaState.cc
+ * \author Lars Froehlich
+ * \date   Created on December 3, 2021
+ * \brief  Test suite for the LuaState class.
+ *
+ * \copyright Copyright 2021 Deutsches Elektronen-Synchrotron (DESY), Hamburg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 2.1 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#include <type_traits>
+#include <gul14/catch.h>
+#include "../include/avtomat/LuaState.h"
+
+using namespace avto;
+
+TEST_CASE("LuaState: Default constructor", "[LuaState]")
+{
+    static_assert(std::is_default_constructible<LuaState>::value,
+        "LuaState is_default_constructible");
+
+    LuaState state;
+}
+
+TEST_CASE("LuaState: Move constructor", "[LuaState]")
+{
+    LuaState state;
+
+    LuaState state2{ std::move(state) };
+    REQUIRE(state.get() == nullptr);
+    REQUIRE(state2.get() != nullptr);
+}
+
+TEST_CASE("LuaState: get()", "[LuaState]")
+{
+    LuaState state1;
+    REQUIRE(state1.get() != nullptr);
+}
+
+TEST_CASE("LuaState: operator=(LuaState&&) (move assignment)", "[LuaState]")
+{
+    LuaState state1;
+    auto* state1_ptr = state1.get();
+    REQUIRE(state1_ptr != nullptr);
+
+    LuaState state2;
+    auto* state2_ptr = state2.get();
+    REQUIRE(state2_ptr != nullptr);
+
+    state2 = std::move(state1);
+    REQUIRE(state1.get() == nullptr);
+    REQUIRE(state2.get() == state1_ptr);
+}
