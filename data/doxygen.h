9e3c33326773c9abcf99aa1fd5e2323ba88aa9cd
Lars Froehlich <lars.froehlich@desy.de>
2021-12-20 13:30:12 +0100

    Add Sol2 library (C++ bindings for LUA)
    [why]
    Sol2 provides all the functionality we would otherwise
    have had to create ourselves in LuaState. It is also
    being actively maintained. We include it in src/ for
    two reasons:
    1. There is no preexisting Ubuntu package for the library
    2. Keeping the header-only library in src/sol allows
       us to use it internally but isolates the user of
       libavtomat from it. In other words, the Sol2 headers
       do not leak into the user's include directories.
    
    Signed-off-by: Lars Froehlich <lars.froehlich@desy.de>

--- data/doxygen.h
+++ data/doxygen.h
@@ -18,7 +18,8 @@
  * \note
  * The Avtomat library requires at least C++17.
  *
- * \author Lars Froehlich (for third-party code see \ref copyright)
+ * \authors Lars Froehlich, Olaf Hensler, Marcus Walla (for third-party code see
+ *          \ref copyright)
  *
  * \copyright
  * See \ref copyright
@@ -52,7 +53,30 @@
  *
  * \section additional_copyright_notices Additional Copyright Notices
  *
- * This library currently contains no third-party code.
+ * This library contains the Sol2 library in the directory src/sol (see
+ * https://github.com/ThePhD/sol2). Sol2 is distributed under the MIT license:
+ *
+ * <dl>
+ * <dt>Sol2 library</dt>
+ * <dd>Copyright (c) 2013-2021 Rapptz, ThePhD, and contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</dd>
+ * </dl>
  */
 
 /**
