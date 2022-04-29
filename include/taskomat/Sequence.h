32f9db79f02686e3b0e15b883ff10a217e19ad41
Marcus Walla <marcus.walla@desy.de>
2022-04-29 17:54:08 +0200

    Correct comment on pop_back() for invalid iterator.

--- include/taskomat/Sequence.h
+++ include/taskomat/Sequence.h
@@ -200,7 +200,7 @@ public:
      * Remove the last element from the sequence.
      * 
      * Removeing on an empty Sequence returns silently. Iterators and references to the 
-     * last element as well as the end(), cend(), rend(), and crend() iterators are marked
+     * last element as well as the end(), and cend() iterators are marked
      * invalid after finishing the function call.
      */
     void pop_back() { if (not steps_.empty()) steps_.pop_back(); indent(); }
