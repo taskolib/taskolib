f04d10e7fe9da159f34c940e8b3755d829e26c94
Marcus Walla <marcus.walla@desy.de>
2022-06-10 15:27:14 +0200

    Adapted deserialize by including a separate file to perform nice switch cases as strings.

--- src/deserialize_sequence.cc
+++ src/deserialize_sequence.cc
@@ -18,30 +18,9 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
- * MIT license
- *  
- * Copyright (c) 2020-22 Richard Spencer
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies
- * or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
- * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-// SPDX-License-Identifier: LGPL-2.1-or-later AND MIT
+// SPDX-License-Identifier: LGPL-2.1-or-later
 
 #include <chrono>
 #include <fstream>
@@ -52,6 +31,7 @@
 #include <ctime>
 #include <string_view>
 #include <gul14/gul.h>
+#include "taskomat/case_string.h"
 #include "taskomat/deserialize_sequence.h"
 
 namespace task {
@@ -59,26 +39,6 @@ namespace task {
 namespace {
 
 static const char task[] = "task";
-
-/// Adapt switch statement with stringify items:
-/// https://learnmoderncpp.com/2020/06/01/strings-as-switch-case-labels/
-/// Version 1.4: 2022/01/15, MIT license, (c) 2020-22 Richard Spencer, see above
-constexpr auto hash(const std::string_view sv)
-{
-    unsigned long hash{ 5381 };
-    for (unsigned char c : sv) {
-        hash = ((hash << 5) + hash) ^ c;
-    }
-    return hash;
-}
- 
-/// Adapt switch statement with stringify items:
-/// https://learnmoderncpp.com/2020/06/01/strings-as-switch-case-labels/
-/// Version 1.4: 2022/01/15, MIT license, (c) 2020-22 Richard Spencer, see above
-constexpr auto operator"" _sh(const char *str, size_t len)
-{
-    return hash(std::string_view{ str, len });
-}
 
 /// Extracted from High Level Controls Utility Library (DESY), file string_util.h/cc
 int hex2dec(const char c)
@@ -149,7 +109,7 @@ void extract_type(const std::string& extract, Step& step)
 
     auto keyword = extract.substr(pos + 2);
 
-    switch(hash(keyword))
+    switch(hash_djb2a(keyword))
     {
         case "action"_sh:
             step.set_type(Step::type_action); break;
@@ -264,8 +224,11 @@ std::istream& operator>>(std::istream& stream, Step& step)
     {
         auto keyword = extract_keyword(extract);
 
-        // Using switch cases with string hash (see operator"" _sh etc)
-        switch(hash(keyword))
+        // Using switch cases with string hashes (see operator"" _sh in case_string.h)
+        // DJB2A hash algorithm: http://www.cse.yorku.ca/%7Eoz/hash.html
+        // Interesting hashing algorithm comparison:
+        // https://softwareengineering.stackexchange.com/questions/49550/which-hashing-algorithm-is-best-for-uniqueness-and-speed/145633#145633
+        switch(hash_djb2a(keyword))
         {
             case "type"_sh:
                 extract_type(extract, step); break;
