e0197739673f3516e6af3b98d4af194f6b638f07
Lars Froehlich <lars.froehlich@desy.de>
2022-06-22 14:58:05 +0200

    Add console output functions
    [why]
    These functions will be used as default implementations
    for the output callbacks in the Context struct for
    print() and log entries.
    
    Signed-off-by: Lars Froehlich <lars.froehlich@desy.de>

---
+++ src/console.cc
@@ -0,0 +1,50 @@
+/**
+ * \file   console.cc
+ * \author Lars Froehlich
+ * \date   Created on June 22, 2022
+ * \brief  Implementation of several functions for console output.
+ *
+ * \copyright Copyright 2022 Deutsches Elektronen-Synchrotron (DESY), Hamburg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 2.1 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#include <iostream>
+#include "taskomat/console.h"
+
+namespace task {
+
+void print_to_stdout(const std::string& str, CommChannel*)
+{
+    std::cout << str;
+}
+
+void print_info_to_stdout(const std::string& str, CommChannel*)
+{
+    std::cout << "INFO: " << str << "\n";
+}
+
+void print_warning_to_stdout(const std::string& str, CommChannel*)
+{
+    std::cout << "WARNING: " << str << "\n";
+}
+
+void print_error_to_stderr(const std::string& str, CommChannel*)
+{
+    std::cerr << "ERROR: " << str << "\n";
+}
+
+} // namespace task
