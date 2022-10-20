0e68aef073f8f3f838ffe314061920ea515d0c74
Marcus Walla <marcus.walla@desy.de>
2022-10-20 14:55:28 +0200

    Oops! Have had a wrong typing window :-( ... now corrected...

--- src/Sequence.cc
+++ src/Sequence.cc
@@ -61,7 +61,7 @@ find_end_of_indented_block(IteratorT begin, IteratorT end, short min_indentation
 
 Sequence::Sequence(gul14::string_view label)
 {
-    set_label(label);git co
+    set_label(label);
 }
 
 void Sequence::set_label(gul14::string_view new_label)
