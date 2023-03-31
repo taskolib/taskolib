36c9c45161dda27980ef57d5325761ba32c3c0d4
Jannik Woehnert <jannik.woehnert@desy.de>
2023-03-31 12:23:57 +0200

    debug status and add, not succesdsful yet

--- tests/test_GitWrapper.cc
+++ tests/test_GitWrapper.cc
@@ -35,158 +35,184 @@ using namespace task;
 
 
 
-TEST_CASE("Construct LibGit object", "[GitWrapper]")
+TEST_CASE("LibGit Wrapper Test all", "[GitWrapper]")
 {
-    // make sure directory is empty
-    std::filesystem::remove_all("sequences");
 
-
-    // prepare first sequence for test
-    Step step_1_01{Step::type_while};
-    step_1_01.set_label("while");
-    step_1_01.set_script("return i < 10");
-
-    Step step_1_02{Step::type_action};
-    step_1_02.set_label("action");
-    step_1_02.set_script("i = i + 1");
-
-    Sequence seq_1{"unit_test_1"};
-    seq_1.push_back(step_1_01);
-    seq_1.push_back(step_1_02);
-
-    store_sequence("sequences", seq_1);
-
-
-    // Create Git Library
-    LibGit gl{"sequences"};
-    
-
-    REQUIRE(not gl.get_path().empty());
-    REQUIRE(gl.get_path() == "sequences");
-    git_reference *temp;
-    REQUIRE(git_repository_head(&temp, gl.get_repo()) == 0);
-    
-    
-    // Test if repo_ got initialized
-    REQUIRE(gl.get_last_commit_message() == "Initial commit");
-
-}
-
-TEST_CASE("Stage files", "[GitWrapper]")
-{
-    // Create Git Library
-    LibGit gl{"sequences"};
-
-    // prepare another sequence for test
-    Step step_2_01{Step::type_while};
-    step_2_01.set_label("while");
-    step_2_01.set_script("return i < 10");
-
-    Step step_2_02{Step::type_action};
-    step_2_02.set_label("action");
-    step_2_02.set_script("i = i + 1");
-
-    Sequence seq_2{"unit_test_2"};
-    seq_2.push_back(step_2_01);
-    seq_2.push_back(step_2_02);
-
-    store_sequence("sequences", seq_2);
-
-    std::vector<std::array<std::string, 3>> stats = gl.libgit_status();
-
-    for (size_t i =0; i < stats.size(); i++)
+    SECTION("Construct LibGit object")
     {
-        std::array<std::string, 3> elm = stats.at(i);
-        if (elm[0] == "unit_test_2/step_1_while.lua")
+        // make sure directory is empty
+        std::filesystem::remove_all("sequences");
+
+        // prepare first sequence for test
+        Step step_1_01{Step::type_while};
+        step_1_01.set_label("while");
+        step_1_01.set_script("return i < 10");
+
+        Step step_1_02{Step::type_action};
+        step_1_02.set_label("action");
+        step_1_02.set_script("i = i + 1");
+
+        Sequence seq_1{"unit_test_1"};
+        seq_1.push_back(step_1_01);
+        seq_1.push_back(step_1_02);
+
+        store_sequence("sequences", seq_1);
+
+
+        
+        // Create Git Library
+        LibGit gl{"sequences"};
+
+        std::vector<std::array<std::string, 3>> stats = gl.libgit_status();
+        REQUIRE(stats.size() != 0);
+        for (size_t i =0; i < stats.size(); i++)
         {
-            REQUIRE(elm[1] == "unstaged");
-            REQUIRE(elm[2] == "new file");
+            std::array<std::string, 3> elm = stats.at(i);
+            if (elm[0] == "unit_test_1/step_1_while.lua")
+            {
+                REQUIRE(elm[1] == "unstaged");
+                REQUIRE(elm[2] == "new file");
+            }
         }
+
+        REQUIRE(not gl.get_path().empty());
+        REQUIRE(gl.get_path() == "sequences");
+        git_reference *temp;
+        REQUIRE(git_repository_head(&temp, gl.get_repo()) == 0);
+        
+        
+        // Test if repo_ got initialized
+        REQUIRE(gl.get_last_commit_message() == "Initial commit");
+
     }
 
 
-    gl.libgit_add();
-
-    stats = gl.libgit_status();
-
-    for (size_t i =0; i < stats.size(); i++)
+    SECTION("Stage files")
     {
-        std::array<std::string, 3> elm = stats.at(i);
-        if (elm[0] == "unit_test_2/step_2_action.lua")
+
+        // prepare another sequence for test
+        Step step_2_01{Step::type_while};
+        step_2_01.set_label("while");
+        step_2_01.set_script("return i < 10");
+
+        Step step_2_02{Step::type_action};
+        step_2_02.set_label("action");
+        step_2_02.set_script("i = i + 1");
+
+        Sequence seq_2{"unit_test_2"};
+        seq_2.push_back(step_2_01);
+        seq_2.push_back(step_2_02);
+
+        store_sequence("sequences", seq_2);
+
+        // Create Git Library
+        LibGit gl{"sequences"};
+
+        std::vector<std::array<std::string, 3>> stats = gl.libgit_status();
+        REQUIRE(stats.size() != 0);
+        for (size_t i =0; i < stats.size(); i++)
         {
-            REQUIRE(elm[1] == "staged");
-            REQUIRE(elm[2] == "new file");
+            std::array<std::string, 3> elm = stats.at(i);
+            if (elm[0] == "unit_test_2/step_1_while.lua")
+            {
+                REQUIRE(elm[1] == "unstaged");
+                REQUIRE(elm[2] == "new file");
+            }
         }
-    }
-
-}
 
 
-TEST_CASE("Commit", "[GitWrapper]")
-{
-    // Create Git Library
-    LibGit gl{"sequences"};
-    
-    
-    // Check if repo_ can be found again
-    REQUIRE(gl.get_last_commit_message() == "Initial commit");
+        gl.libgit_add();
 
-    gl.libgit_add(); // add unit_test_2
-    gl.libgit_commit("Add second sequence");
+        stats = gl.libgit_status();
 
-    REQUIRE(gl.get_last_commit_message() == "Add second sequence");
-
-    auto stats = gl.libgit_status();
-
-    for (size_t i =0; i < stats.size(); i++)
-    {
-        std::array<std::string, 3> elm = stats.at(i);
-        REQUIRE(elm[1] != "staged");
-
-    }
-
-}
-
-
-TEST_CASE("Delete Sequence", "[GitWrapper]")
-{
-
-    // Create Git Library
-    LibGit gl{"sequences"};
-    
-    std::filesystem::path mypath = "unit_test_2";
-
-    gl.libgit_remove_sequence(mypath);
-
-    auto stats = gl.libgit_status();
-
-    for (size_t i =0; i < stats.size(); i++)
-    {
-        std::array<std::string, 3> elm = stats.at(i);
-        if (elm[0] == "unit_test_2/step_2_action.lua")
+        // new submodules from unit_test_2 should be in stage mode
+        REQUIRE(stats.size() != 0);
+        for (size_t i =0; i < stats.size(); i++)
         {
-            REQUIRE(elm[1] != "unstaged");
-            REQUIRE(elm[2] != "deleted");
+            std::array<std::string, 3> elm = stats.at(i);
+            if (elm[0] == "unit_test_2/step_2_action.lua")
+            {
+                REQUIRE(elm[1] == "staged");
+                REQUIRE(elm[2] == "new file");
+            }
         }
 
     }
 
-    gl.libgit_add();
 
-    stats = gl.libgit_status();
-
-    for (size_t i =0; i < stats.size(); i++)
+    SECTION("Commit")
     {
-        std::array<std::string, 3> elm = stats.at(i);
-        if (elm[0] == "unit_test_2/step_2_action.lua")
+        // Create Git Library
+        LibGit gl{"sequences"};
+        
+        
+        // Check if repo_ can be found again
+        REQUIRE(gl.get_last_commit_message() == "Initial commit");
+
+        gl.libgit_add(); // add unit_test_2
+        gl.libgit_commit("Add second sequence");
+
+        REQUIRE(gl.get_last_commit_message() == "Add second sequence");
+
+        // get status
+        std::vector<std::array<std::string, 3>> stats = gl.libgit_status();
+
+        // No submodule should be staged
+        REQUIRE(stats.size() != 0);
+        for (size_t i =0; i < stats.size(); i++)
         {
+            std::array<std::string, 3> elm = stats.at(i);
             REQUIRE(elm[1] != "staged");
-            REQUIRE(elm[2] != "deleted");
+
         }
 
     }
 
-    // check if path got removed
-    REQUIRE(not std::filesystem::exists("sequences" / mypath));
 
+    SECTION("Delete Sequence")
+    {
+        // Create Git Library
+        LibGit gl{"sequences"};
+        
+        std::filesystem::path mypath = "unit_test_2";
+
+        gl.libgit_remove_sequence(mypath);
+
+        std::vector<std::array<std::string, 3>> stats = gl.libgit_status();
+
+        // every file in unit_test_2 should have the tag deleted
+        REQUIRE(stats.size() != 0);
+        for (size_t i =0; i < stats.size(); i++)
+        {
+            
+            std::array<std::string, 3> elm = stats.at(i);
+            if (elm[0] == "unit_test_2/step_2_action.lua")
+            {
+                REQUIRE(elm[1] == "unstaged");
+                REQUIRE(elm[2] == "deleted");
+            }
+
+        }
+
+        gl.libgit_add();
+
+        stats = gl.libgit_status();
+        REQUIRE(stats.size() != 0);
+        for (size_t i =0; i < stats.size(); i++)
+        {
+            std::array<std::string, 3> elm = stats.at(i);
+            if (elm[0] == "unit_test_2/step_2_action.lua")
+            {
+                REQUIRE(elm[1] == "staged");
+                REQUIRE(elm[2] == "deleted");
+            }
+
+        }
+
+        gl.libgit_commit("remove sequence");
+
+        // check if path got removed
+        REQUIRE(not std::filesystem::exists("sequences" / mypath));
+
+    }
 }
