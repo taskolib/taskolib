3c4ce93b599656fea76e858a98a1c98dbd5b3dac
Jannik Woehnert <jannik.woehnert@desy.de>
2023-04-14 16:42:29 +0200

    ressolve more reviews from Lars and get status of unchanged and untracked files

--- include/taskolib/GitObjectWrapper.h
+++ include/taskolib/GitObjectWrapper.h
@@ -38,13 +38,14 @@ void free_libgit_ptr(git_index* index);
 void free_libgit_ptr(git_repository* repo);
 void free_libgit_ptr(git_remote* remote);
 void free_libgit_ptr(git_commit* commit);
+void free_libgit_ptr(git_status_list* status);
 
 
 /**
  * Template class wrapper for all libgit2 pointer types starting with git_{a-z}*.
  * Copy methods are excluded to prevent double ownership of said pointer.
  * 
- * \tparam T: A pointer type like git_remote*, git_tree*, git_commit*, ...
+ * \tparam T: A dereferencing from a pointer type like git_remote*, git_tree*, git_commit*, ...
 */
 template <class T>
 class LibGitPointer
@@ -76,15 +77,20 @@ public:
     /// (copy assignment) Shall not be used to prevent double ownership of pointer.
     LibGitPointer& operator=(const LibGitPointer&) = delete;
 
-    /// function for member variables of GitRepository object
+    /**
+     * function to free member variables of GitRepository object before deconstruction of their owner.
+     * necessary because they have to be freed before the libgit2 environment deconstruction, see:
+     * git_libgit2_shutdow()
+     */
     void reset()
     {
         free_libgit_ptr(val_);
         val_ = nullptr;
     }
 
-    /// getter functions for const and non-const
+    /// Return a non-owning pointer to the managed object (or nullptr if no object is managed).
     T* get() {return val_;} ;
+    /// Return a constant non-owning pointer to the managed object (or nullptr if no object is managed).
     const T* get() const {return val_;};
 
 private:
@@ -92,7 +98,6 @@ private:
 };
 
 
-//TODO: This works in doxygen according to documentation
 /**
  * Function wrappers for libgit2 functions which manipulates pointers.
  * (Which take double pointers as parameters)
@@ -103,7 +108,7 @@ private:
  */
 
 /**
- * Returns an existing repository
+ * Return an existing repository.
  * \param repo_path absolute or relative path from executable
  * \return Wrapper of a git_repository*
 */
@@ -111,27 +116,28 @@ LibGitPointer<git_repository> repository_open   (const std::string& repo_path);
 
 
 /**
- * Returns a fresh initialized repository
+ * Return a fresh initialized repository.
  * \param repo_path absolute or relative path from executable
  * \return Wrapper of a git_repository*
 */
 LibGitPointer<git_repository> repository_init   (const std::string& repo_path, bool is_bare);
 
 /**
- * Returns the current index of a repository
+ * Return the current index of a repository.
  * \param repo C-type repository
  * \return Wrapper of a git_index*
 */
 LibGitPointer<git_index>      repository_index  (git_repository* repo);
 
 /**
- * Generates a signature from system values (already collected by the C-type repository)
+ * Generate a signature from system values (already collected by the C-type repository).
  * \param repo_path absolute or relative path from executable
  * \return Wrapper of a git_signature*
 */
 LibGitPointer<git_signature>  signature_default (git_repository* repo);
+
 /**
- * Generates a signature from given parameters
+ * Generate a signature from given parameters.
  * \param name User name who creates the commit
  * \param email E-mail of the user
  * \param time timestamp
@@ -143,11 +149,18 @@ LibGitPointer<git_signature>  signature_new     (const std::string& name, const 
 
 
 /**
- * Collects the current tree of the git repository
+ * Collect the current tree of the git repository.
  * \param repo C-type repository
  * \param tree_id identity number of the active tree
  * \return Wrapper of a git_signature*
 */
 LibGitPointer<git_tree>       tree_lookup       (git_repository* repo, git_oid tree_id);
+
+/**
+ * Create a new status list which conatins status_list_entry* elements
+ * \param repo C-type repository
+ * \param status_opt struct of status options
+*/
+LibGitPointer<git_status_list> status_list_new  (git_repository* repo, const git_status_options status_opt);
 /** \}*/
 }
--- tests/test_GitRepository.cc
+++ tests/test_GitRepository.cc
@@ -1,10 +1,10 @@
 /**
- * \file   test_GitWrapper.cc
+ * \file   test_GitRepository.cc
  * \author Sven-Jannik Woehnert
  * \date   Created on March 22, 2023
- * \brief  Test suite for the GitWrapper class.
+ * \brief  Test suite for the GitRepository class.
  *
- * \copyright Copyright 2022 Deutsches Elektronen-Synchrotron (DESY), Hamburg
+ * \copyright Copyright 2023 Deutsches Elektronen-Synchrotron (DESY), Hamburg
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -30,35 +30,51 @@
 #include "taskolib/Step.h"
 #include "taskolib/Sequence.h"
 #include <filesystem>
+#include <fstream>
 
 using namespace task;
 
 
+/**
+ *  Creates a directory and store files in it.
+ * Filestructure: \n
+ * sequences/     \n
+ *      $name$/   \n
+ *          file0.txt   << $msg$ /n file0  \n
+ *          file1.txt   << $msg$ /n file1  \n
+ *          ...
+ * \param name name of the directory
+ * \param nr_files number of files to be created
+ * \param msg What to write to the file
+ */
+void create_testfiles(const std::filesystem::path& name, size_t nr_files, const std::string& msg)
+{
+    std::filesystem::path p = "sequences" / name;
+    std::filesystem::create_directories(p);
+    for (size_t i = 0; i < nr_files; i++)
+    {
+        // msg
+        // file i
+        std::ofstream f(p/gul14::cat("file", i, ".txt"));
+        f << msg << gul14::cat("\nfile", i);
+    }  
+}
+
 
 TEST_CASE("GitRepository Wrapper Test all", "[GitWrapper]")
 {
-
-
+    /**
+     * Create files in a directory and then initialize the git repository within.
+     * The initial commit must be empty as no staging has been done.
+     * Check for the initialization of member variables
+     * Check if a repository is created (HEAD exists)
+     * Check if a comit was created (initial commit)
+     * 
+     */
     SECTION("Construct GitRepository object")
     {
+        create_testfiles("unit_test_1", 2, "Construct");
 
-        // prepare first sequence for test
-        Step step_1_01{Step::type_while};
-        step_1_01.set_label("while");
-        step_1_01.set_script("return i < 6");
-
-        Step step_1_02{Step::type_action};
-        step_1_02.set_label("action");
-        step_1_02.set_script("i = i + 5");
-
-        Sequence seq_1{"unit_test_1"};
-        seq_1.push_back(step_1_01);
-        seq_1.push_back(step_1_02);
-
-        store_sequence("sequences", seq_1);
-
-
-        
         // Create Git Library
         GitRepository gl{"sequences"};
 
@@ -70,33 +86,34 @@ TEST_CASE("GitRepository Wrapper Test all", "[GitWrapper]")
         
         // Test if repo_ got initialized
         REQUIRE(gl.get_last_commit_message() == "Initial commit");
-
     }
 
 
+    /**
+     * Check the general stag function "add".
+     * 1) Load existing repository (in contrast to initialize in first section)
+     * 2) Create files after repository loading (in contrast to first section)
+     * 3) Check if files appear as untracked
+     * 4) Stage all files. Now the status should show 4 staged new files
+     */
     SECTION("Stage files")
     {
-
-        // prepare another sequence for test
-        Step step_2_01{Step::type_while};
-        step_2_01.set_label("while");
-        step_2_01.set_script("return i < 10");
-
-        Step step_2_02{Step::type_action};
-        step_2_02.set_label("action");
-        step_2_02.set_script("i = i + 1");
-
-        Sequence seq_2{"unit_test_2"};
-        seq_2.push_back(step_2_01);
-        seq_2.push_back(step_2_02);
-
-        store_sequence("sequences", seq_2);
-
         // Create Git Library
         GitRepository gl{"sequences"};
 
+        create_testfiles("unit_test_2", 2, "Stage");
+
         std::vector<FileStatus> stats = gl.status();
-        REQUIRE(stats.size() == 0);
+        REQUIRE(stats.size() != 0);
+        for (size_t i =0; i < stats.size(); i++)
+        {
+            FileStatus elm = stats.at(i);
+            if (elm.path_name.rfind("unit_test_1", 0) == 0 || elm.path_name.rfind("unit_test_2", 0) == 0)
+            {
+                REQUIRE(elm.handling == "untracked");
+                REQUIRE(elm.changes == "untracked");
+            }
+        }
 
         gl.add();
 
@@ -113,81 +130,123 @@ TEST_CASE("GitRepository Wrapper Test all", "[GitWrapper]")
                 REQUIRE(elm.changes == "new file");
             }
         }
-
     }
 
-
+    /**
+     * Commit the previous staged files.
+     * 1) Load repository. 4 files should still be staged
+     * 2) Last commit should be the initial commit
+     * 3) commit staged files. files should now be in status "unchanged"
+     * 4) Check if commit message was set successful
+     */
     SECTION("Commit")
     {
+
         // Create Git Library
         GitRepository gl{"sequences"};
-        
+
+        auto stats = gl.status();
+
+        // new submodules from unit_test_2 should be still in stage mode
+        REQUIRE(stats.size() != 0);
+        for (size_t i =0; i < stats.size(); i++)
+        {
+            FileStatus elm = stats.at(i);
+            if (elm.path_name.rfind("unit_test_1", 0) == 0 || elm.path_name.rfind("unit_test_2", 0) == 0)
+            {
+                REQUIRE(elm.handling == "staged");
+                REQUIRE(elm.changes == "new file");
+            }
+        }
         
         // Check if repo_ can be found again
         REQUIRE(gl.get_last_commit_message() == "Initial commit");
 
-        gl.add(); // add unit_test_2
         gl.commit("Add second sequence");
 
-        REQUIRE(gl.get_last_commit_message() == "Add second sequence");
+        stats = gl.status();
+        REQUIRE(stats.size() != 0);
+              for (size_t i =0; i < stats.size(); i++)
+        {
+            FileStatus elm = stats.at(i);
+            if (elm.path_name.rfind("unit_test_1", 0) == 0 || elm.path_name.rfind("unit_test_2", 0) == 0)
+            {
+                REQUIRE(elm.handling == "unchanged");
+                REQUIRE(elm.changes == "unchanged");
+            }
+        }
 
+        REQUIRE(gl.get_last_commit_message() == "Add second sequence");
     }
 
+
+    /**
+     * Change two files, but only stage one of them.
+     * 1) Manipulate both files of unit_test_1
+     * 2) check if status of them is modified, but unstaged
+     * 3) stage file1 of unit_test_1
+     * 4) file1 should be staged and file0 still be unstaged
+    */
     SECTION("Add by path")
     {
         GitRepository gl{"sequences"};
 
-        // change unit_test_2
-        Step step_2_01{Step::type_while};
-        step_2_01.set_label("while");
-        step_2_01.set_script("return i >= 10");
-
-        Step step_2_02{Step::type_action};
-        step_2_02.set_label("action");
-        step_2_02.set_script("print('hello')");
-
-        Sequence seq_2{"unit_test_1"};
-        seq_2.push_back(step_2_01);
-        seq_2.push_back(step_2_02);
-
-        store_sequence("sequences", seq_2);
+        create_testfiles("unit_test_1", 2, "Add by path");
 
         std::vector<FileStatus> stats = gl.status();
         REQUIRE(stats.size() != 0);
         for (size_t i =0; i < stats.size(); i++)
         {
             FileStatus elm = stats.at(i);
-            if (elm.path_name.rfind("unit_test_1/step", 0) == 0)
+            if (elm.path_name.rfind("unit_test_1/file", 0) == 0)
             {
                 REQUIRE(elm.handling == "unstaged");
                 REQUIRE(elm.changes == "modified");
             }
+            else if (elm.path_name.rfind("unit_test_2/file", 0) == 0)
+            {
+                REQUIRE(elm.handling == "unchanged");
+                REQUIRE(elm.changes == "unchanged");
+            }
         }
 
-        auto ret = gl.add_files({"unit_test_1/step_2_action.lua"});
+        auto ret = gl.add_files({"unit_test_1/file1.txt"});
+
+        // No errors should have occur 
+        REQUIRE(ret.size() == 0);
 
         stats = gl.status();
         REQUIRE(stats.size() != 0);
         for (size_t i =0; i < stats.size(); i++)
         {
             FileStatus elm = stats.at(i);
-            if (elm.path_name.rfind("unit_test_1/step_1", 0) == 0)
+            if (elm.path_name.rfind("unit_test_1/file0", 0) == 0)
             {
                 REQUIRE(elm.handling == "unstaged");
                 REQUIRE(elm.changes == "modified");
             }
-            else if (elm.path_name.rfind("unit_test_1/step_2", 0) == 0)
+            else if (elm.path_name.rfind("unit_test_1/file1", 0) == 0)
             {
                 REQUIRE(elm.handling == "staged");
                 REQUIRE(elm.changes == "modified");
             }
+            else if (elm.path_name.rfind("unit_test_2/file", 0) == 0)
+            {
+                REQUIRE(elm.handling == "unchanged");
+                REQUIRE(elm.changes == "unchanged");
+            }
         }
-
-
     }
 
-
-    SECTION("Delete Sequence")
+    /**
+     * remove a directory and check if the repository status notices
+     * 1) Delete unit_test_2
+     * 2) Files should be automatically staged for deletion
+     * 3) Commit removal
+     * 4) check if files are gone from status
+     * 5) check if files are gone from filesystem
+     */
+    SECTION("Delete Directory")
     {
         // Create Git Library
         GitRepository gl{"sequences"};
@@ -199,7 +258,7 @@ TEST_CASE("GitRepository Wrapper Test all", "[GitWrapper]")
         std::vector<FileStatus> stats = gl.status();
 
         // every file in unit_test_2 should have the tag deleted
-        //REQUIRE(stats.size() != 0);
+        REQUIRE(stats.size() != 0);
         for (size_t i =0; i < stats.size(); i++)
         {
             
@@ -209,14 +268,22 @@ TEST_CASE("GitRepository Wrapper Test all", "[GitWrapper]")
                 REQUIRE(elm.handling == "staged");
                 REQUIRE(elm.changes == "deleted");
             }
-
         }
 
         gl.commit("remove sequence");
 
+        stats = gl.status();
+
+        // check if files are removed from repository status
+        REQUIRE(stats.size() != 0);
+        for (size_t i =0; i < stats.size(); i++)
+        {
+            FileStatus elm = stats.at(i);
+            REQUIRE (elm.path_name.rfind("unit_test_2/file", 0) != 0);
+        }
+
         // check if path got removed
         REQUIRE(not std::filesystem::exists("sequences" / mypath));
-
 
         std::filesystem::remove_all("sequences");
     }
